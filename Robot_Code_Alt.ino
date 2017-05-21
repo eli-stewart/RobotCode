7c82d9cc0e4e94351be365b95ca63b362300e097
eli-stewart <eliwstewart10@gmail.com>
2017-05-21 16:38:40 -0700

    Lots of stuffs.

--- Robot_Code_Alt.ino
+++ Robot_Code_Alt.ino
@@ -16,15 +16,29 @@ const int frontSwitchPowerPin = 8;
 
 // Motor speeds.
 const int defaultMotorSpeed = 100;
-const int correctionMotorSpeed = 150;
+const int correctionMotorSpeed = 255;
 
 // Switch states.
 const int pressed = LOW;
 const int unpressed = HIGH;
 
+// Recent whisker.
+int recentAction = -1;
+
+// Turning.
+bool turning = false;
+
+// Timers.
+unsigned long nextTimeLeft;
+unsigned long nextTimeRight;
+unsigned long nextTimeTurn;
+bool timingRight = false;
+bool timingLeft = false;
+bool timingTurn = false;
+
 void setup() {
   Serial.begin(9600); // Set up Serial library at 9600 bps.
-  AFMS.begin(); // Create with the default frequency 1.6KHz.
+  AFMS.begin(); // Create with the default frequency 1.6KHz
 
   // Setup pins.
   pinMode(leftSwitchInputPin, INPUT);
@@ -45,44 +59,205 @@ void loop() {
   int rightSwitchPressed = digitalRead(rightSwitchInputPin);
   int frontSwitchPressed = digitalRead(frontSwitchInputPin);
 
-  if (frontSwitchPressed == LOW) {
+  if (!turning ) {
     if (leftSwitchPressed == pressed && rightSwitchPressed == pressed) {
-      Serial.print("F");
-      leftMotor->setSpeed(defaultMotorSpeed);
-      rightMotor->setSpeed(defaultMotorSpeed);
-      leftMotor->run(FORWARD);
-      rightMotor->run(FORWARD);
+      //turning = false;
+      driveForward();
     }
     else if (leftSwitchPressed == unpressed && rightSwitchPressed == pressed) {
-      Serial.print("L");
-      leftMotor->setSpeed(correctionMotorSpeed);
-      rightMotor->setSpeed(defaultMotorSpeed);
-      leftMotor->run(FORWARD);
-      rightMotor->run(FORWARD);
+      // turning = false;
+      correctLeft();
+      timerLeft();
     }
     else if (leftSwitchPressed == pressed && rightSwitchPressed == unpressed) {
-      Serial.print("R");
-      leftMotor->setSpeed(defaultMotorSpeed);
-      rightMotor->setSpeed(correctionMotorSpeed);
-      leftMotor->run(FORWARD);
-      rightMotor->run(FORWARD);
+      //turning = false;
+      correctRight();
+      timerRight();
     }
     else if (leftSwitchPressed == unpressed && rightSwitchPressed == unpressed) {
-      Serial.print("S");
-      leftMotor->run(RELEASE);
-      rightMotor->run(RELEASE);
+      if (recentAction == 2) {
+        driveForward();
+      }
+      else if (recentAction == 0) {
+        // Most recent whisker is left. (Robot was correcting right.)
+        pivotLeft();
+      }
+      else if (recentAction == 1) {
+        // Most recent whisker is right. (Robot was correcting left.)
+        pivotRight();
+      }
+
     }
   }
-  else if (frontSwitchPressed == HIGH) {
-    // TODO.
+  // Turning.
+  if (frontSwitchPressed == HIGH) {
+
+    turning = true;
+  }
+  if (turning) {
+    turn(leftSwitchPressed, rightSwitchPressed, frontSwitchPressed);
   }
 }
 
-void turnLeft() {
-  Serial.print("L");
+void turn(int leftSwitchPressed, int rightSwitchPressed, int frontSwitchPressed) {
+  timerTurn();
+  if (timingTurn) {
+    if (leftSwitchPressed == pressed) {
+      turnRight();
+    }
+    else if (rightSwitchPressed == pressed) {
+      turnLeft();
+    }
+    else if (frontSwitchPressed == HIGH && rightSwitchPressed == unpressed && leftSwitchPressed == unpressed){
+        if (recentAction == 0) {
+        // Most recent whisker is left. (Robot was correcting right.)
+        pivotLeft();
+      }
+      else if (recentAction == 1) {
+        // Most recent whisker is right. (Robot was correcting left.)
+        pivotRight();
+      }
+    }
+ 
+    else {
+
+      if (recentAction == 1) {
+        // Most recent whisker is right. (Robot was correcting left.)
+        pivotLeft();
+
+      }
+      else if (recentAction == 0) {
+        // Most recent whisker is left. (Robot was correcting right.)
+        pivotRight();
+      }
+    }
+  }
 }
 
 void turnRight() {
-  Serial.print("R");
+  Serial.print("[TR]");
+  releaseMotors();
+  leftMotor->setSpeed(255);
+  leftMotor->run(FORWARD);
+  rightMotor->setSpeed(120);
+  rightMotor->run(BACKWARD);
+}
+
+void turnLeft() {
+  Serial.print("[TL]");
+  releaseMotors();
+  leftMotor->setSpeed(255);
+  leftMotor->run(BACKWARD);
+  rightMotor->setSpeed(255);
+  rightMotor->run(FORWARD);
+}
+
+void pivotLeft() {
+  Serial.print("[PL]");
+  releaseMotors();
+  leftMotor->setSpeed(255);
+  leftMotor->run(BACKWARD);
+  rightMotor->setSpeed(120);
+  rightMotor->run(FORWARD);
+}
+
+void pivotRight() {
+  Serial.print("[PR]");
+  releaseMotors();
+  leftMotor->setSpeed(255);
+  leftMotor->run(FORWARD);
+  rightMotor->setSpeed(120);
+  rightMotor->run(BACKWARD);
+}
+
+void driveForward() {
+  Serial.print("[F]");
+  leftMotor->setSpeed(defaultMotorSpeed);
+  rightMotor->setSpeed(defaultMotorSpeed);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+}
+
+void correctRight() {
+  Serial.print("[CR]");
+  releaseMotors();
+  leftMotor->setSpeed(correctionMotorSpeed);
+  rightMotor->setSpeed(defaultMotorSpeed);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+}
+
+void correctLeft() {
+  releaseMotors();
+  Serial.print("[CL]");
+  leftMotor->setSpeed(defaultMotorSpeed);
+  rightMotor->setSpeed(correctionMotorSpeed);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  //Serial.print (recentAction);
+  // 1 means was correcting left
+}
+
+void releaseMotors() {
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+}
+
+void timerRight() {
+  unsigned long currentTime = millis();
+  if (!timingRight) {
+    nextTimeRight = currentTime + 1000;
+    timingRight = true;
+    //Serial.println("Started timer.");
+  }
+  else if (timingRight) {
+    if (currentTime > nextTimeRight - 100 && currentTime < nextTimeRight + 100) {
+      //Serial.println("Stopped timer.");
+      // 0 means robot was correcting right.
+      recentAction = 0;
+      //Serial.print(recentAction);
+      timingRight = false;
+    }
+    else if (currentTime > nextTimeRight + 100) {
+      timingRight = false;
+    }
+  }
+}
+
+void timerLeft() {
+  unsigned long currentTime = millis();
+  if (!timingLeft) {
+    nextTimeLeft = currentTime + 5000;
+    timingLeft = true;
+    //Serial.println("Started timer.");
+  }
+  else if (timingLeft) {
+    if (currentTime > nextTimeLeft - 100 && currentTime < nextTimeLeft + 100) {
+      //Serial.println("Stopped timer.");
+      // 0 means robot was correcting right.
+      recentAction = 1;
+      //Serial.print(recentAction);
+      timingLeft = false;
+    }
+    else if (currentTime > nextTimeLeft + 100) {
+      timingLeft = false;
+    }
+  }
+}
+
+void timerTurn() {
+  unsigned long currentTime = millis();
+  if (!timingTurn) {
+    nextTimeTurn = currentTime + 1000;
+    timingTurn = true;
+    //Serial.println("Started timer.");
+  }
+  else if (timingTurn) {
+    if (currentTime > nextTimeTurn + 100) {
+      timingTurn = false;
+      turning = false;
+          recentAction = 2;
+    }
+  }
 }
 
